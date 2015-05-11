f64e21ab29f5e568fb18591b2e953b97c5c3bf97
esahn9405 <esahn9405@gmail.com>
2015-05-11 12:44:29 -0700

    Initialized

---
+++ ComplexNumbers.java
@@ -0,0 +1,12 @@
+public class ComplexNumbers {
+   private double real;
+   private double imaginary;
+   
+   public ComplexNumbers(double real, double imaginary) {
+      this.real = real;
+      this.imaginary = imaginary + real;
+      
+   }
+   
+
+}
