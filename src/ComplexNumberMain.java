a1f4f05994ddf64314ca646f097e11c60088801f
esahn9405 <esahn9405@gmail.com>
2015-05-17 02:47:40 -0700

    Initialized

--- src/ComplexNumberMain.java
+++ src/ComplexNumberMain.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class ComplexNumberMain {
    
    public static void main(String[] args) {
-      ComplexNumber c = new ComplexNumber(0, -2);
-      c.printC();
+      ComplexNumber c = new ComplexNumber(0, 2);
+      c.printP();
       Queue<ComplexNumber> numbers = c.findRoot(3);
       ComplexNumber x = numbers.remove();
       x.printC();
@@ -12,5 +12,6 @@ public class ComplexNumberMain {
       x.compareTo(c);
       numbers.remove().printC();
       numbers.remove().printC();
+      System.out.print(1/5.0);
    }
 }
